0a149cf53671b0428db6cb40a49767d5195630a7
Nanateng <nanateng2020@gmail.com>
2022-07-14 18:31:48 +0800

    update

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,31 @@ window.onload = function () {
   document.querySelector('.back').style.display = 'none';
 };
 document.querySelector('.day-photo').addEventListener('click', getFetch);
+document.querySelector('.get-old').addEventListener('click', getPrevious);
 
 function getFetch() {
   const defaultURL = 'https://api.nasa.gov/planetary/apod?api_key=IWQZdLLiNAXbXxaGckrhdK2b98Itw0e4OsyRlK1Q';
 
   fetch(defaultURL)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data);
+      document.querySelector('.results').style.position = 'absolute';
+      document.querySelector('.results').style.display = 'flex';
+      document.querySelector('.back').style.display = 'block';
+      document.querySelector('.reveal').src = data.hdurl;
+      document.querySelector('p').innerHTML = data.explanation;
+    })
+    .catch(err => {
+      console.log(`error ${err}`);
+    });
+}
+
+function getPrevious() {
+  const date = document.querySelector('input').value.toLowerCase();
+  const getOldURL = `https://api.nasa.gov/planetary/apod?api_key=IWQZdLLiNAXbXxaGckrhdK2b98Itw0e4OsyRlK1Q&date=${date}`;
+
+  fetch(getOldURL)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data);
